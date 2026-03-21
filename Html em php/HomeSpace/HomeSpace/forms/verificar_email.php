df00ecac594ae7983eabf0ffed4feb4e7328d270
Hugo Dallabeneta <dallabenete@gmail.com>
2026-03-21 21:02:56 +0000

    Começo do arquivo que o AJAX manda

--- Html em php/HomeSpace/HomeSpace/forms/verificar_email.php
+++ Html em php/HomeSpace/HomeSpace/forms/verificar_email.php
@@ -1,8 +1,16 @@
 <?php 
 
 include("../conexao.php");
+include("../assets/class/utilizador.php");
 
 $email = $_POST["email"];
-echo $email;
+
+$utilizador = new utilizador(null,null,null,$email,null,null,null);
+if ($utilizador->Validacao_De_Recuperacao_De_Senha($email,$conn) === true){
+    ?><h1>boa</h1> <?php
+} else {
+    ?><h1>má</h1> <?php
+}
+
 
 ?>
