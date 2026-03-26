78b92a3b1d8faee70fc78fd236df9883deec4717
Hugo Dallabeneta <dallabenete@gmail.com>
2026-03-26 15:18:25 +0000

    Adiciona uma variável e um parâmetros que que farrá o limite de dados que vão chegar

--- Html em php/HomeSpace/HomeSpace/assets/class/exibir_agentes.php
+++ Html em php/HomeSpace/HomeSpace/assets/class/exibir_agentes.php
@@ -9,12 +9,25 @@
         public string $telemovel
     ){}
 
-    public function DadosAgentes($conexao){
-        $agentes = "SELECT a.ID_Agentes,a.nome , a.NdeTelemovel , a.Servicos , a.Email , a.Imagem , i.concelho FROM agentes a , imoveis i WHERE i.Agentes_ID_Agentes=a.ID_Agentes AND a.disponibilidades = 1 LIMIT 4";
+    public function DadosAgentes($conexao,$limite = null){
+        $agentes = "SELECT a.ID_Agentes, a.nome, a.NdeTelemovel, a.Servicos, a.Email, a.Imagem, i.concelho 
+            FROM agentes a 
+            INNER JOIN imoveis i ON i.Agentes_ID_Agentes = a.ID_Agentes 
+            WHERE a.disponibilidades = 1 
+            GROUP BY a.ID_Agentes";
+
+        // Se o limite for passado , adicionamos à query
+        if($limite !== null && is_int($limite)){
+            $agentes .= " LIMIT " . $limite;
+        }
+
         $stmt = $conexao->prepare($agentes);
         $stmt->execute();
         return $stmt->get_result();
     }
+    }
 
+    public function Imoveis_Do_Agentes($Id_Agentes){
+        
     }
 ?>
