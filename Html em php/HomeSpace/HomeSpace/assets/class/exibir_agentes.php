c17639f260741472619aef1ebd966f845308dd15
Hugo Dallabeneta <dallabenete@gmail.com>
2026-03-30 12:34:32 +0100

    Adição do método AgenteUnico

--- Html em php/HomeSpace/HomeSpace/assets/class/exibir_agentes.php
+++ Html em php/HomeSpace/HomeSpace/assets/class/exibir_agentes.php
@@ -25,7 +25,36 @@
         $stmt->execute();
         return $stmt->get_result();
     }
+
+    public function AgenteUnico($conexao,$id){
+        $agentes_e_imoveis = "SELECT a.nome, a.NdeTelemovel, a.Servicos, a.Email, a.Imagem,a.Servicos, i.concelho 
+            FROM agentes a , imoveis i
+            WHERE a.Id_Agentes = i.Agentes_ID_Agentes AND a.disponibilidades = 1 AND a.ID_Agentes = $id";
+
+        $stmt = $conexao->prepare($agentes_e_imoveis);
+        $stmt->execute();
+        return $stmt->get_result();
     }
 
-    
+    public function contagemDeImoveis($conexao,$id){
+        $Sql_contagem_imovel = "SELECT COUNT(*) as total FROM imoveis WHERE Agentes_ID_Agentes = $id";
+        $stmt = $conexao->prepare($Sql_contagem_imovel);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        $linha = $resultado->fetch_assoc();
+        return $linha['total'];
+
+    }
+
+    public function contagemDeVisitasFeitas($conexao,$id){
+        $Sql_contagem_visitas = "SELECT COUNT(*) as total_visitas FROM utilizador_has_imoveis WHERE Agentes_ID_Agentes = $id";
+        $stmt = $conexao->prepare($Sql_contagem_visitas);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        $linha = $resultado->fetch_assoc();
+        return $linha['total_visitas'];
+    }
+
+
+    }
 ?>
