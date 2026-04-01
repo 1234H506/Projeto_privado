962907502c08cf560c638a1d222c8e4fff24b167
Hugo Dallabeneta <dallabenete@gmail.com>
2026-04-01 11:56:47 +0100

    Class feita para os imóvel

---
+++ Html em php/HomeSpace/HomeSpace/assets/class/Imovel.php
@@ -0,0 +1,20 @@
+<?php 
+class Imovel {
+
+    public function __construct(
+        private ? int $id,
+        private ? string $imagem_principal,
+        private ? string $moradia, 
+        private ? int $preco,
+        private ? string $tipologia,
+        private ? string $dt_de_registro,
+        private ? string $comentario,
+        private ? float $area,
+        private ? string $estado
+
+    )
+    {}
+
+    
+}
+?> 
