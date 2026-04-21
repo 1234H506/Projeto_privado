5efc32ed6ccdfe0a55db6c46bd7b41831774f1af
Hugo Dallabeneta <dallabenete@gmail.com>
2026-04-21 10:01:44 +0100

    Adição de um método na class visita

--- Html em php/HomeSpace/HomeSpace/assets/class/visitas.php
+++ Html em php/HomeSpace/HomeSpace/assets/class/visitas.php
@@ -23,6 +23,13 @@ class visitas{
 
         return ($row1['total']/$row2['total'])*100;
     }
+    public function pending_visits($id_utilizador){
+        $sql = "SELECT * FROM `utilizador_has_imoveis` uhi , imoveis i , agentes a , utilizador u WHERE uhi.Utilizador_ID_Utilizador=u.ID_Utilizador AND uhi.Imoveis_ID_Imoveis = i.ID_Imoveis AND uhi.Agentes_ID_Agentes = a.ID_Agentes AND uhi.Utilizador_ID_Utilizador = $id_utilizador AND uhi.data >= NOW() AND uhi.Status_de_visita != 'cancelado'";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 }
     
 ?>
